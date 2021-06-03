8f4332629f9cc9a95b60efaeb334443afb5e44c8
Branko Visser <b.visser@kwigy.com>
2021-06-03 15:34:50 +0200

    test StoreUser verwijderd

--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -32,9 +32,7 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
 
-        User user = new User("penis", "De Cock", "student");
-        UserDAO userDAO = new UserDAO(getDBaccess());
-        userDAO.storeOne(user);
+
 
 //        DBAccess dbAccess1 = getDBaccess();
 //        dbAccess1.openConnection();
