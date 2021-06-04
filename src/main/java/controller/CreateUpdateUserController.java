eb9d580326da891514fb46187d9080ec2bd50731
Branko Visser <b.visser@kwigy.com>
2021-06-04 21:52:12 +0200

    user verder proberen af te maken, gestuit op wat issues...

--- src/main/java/controller/CreateUpdateUserController.java
+++ src/main/java/controller/CreateUpdateUserController.java
@@ -1,18 +1,32 @@
 package controller;
 
+/*
+* @Author Branko
+*
+* UNDER CONSTRUCTION!
+* */
+
 import database.mysql.DBAccess;
 import database.mysql.UserDAO;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import model.User;
+import view.Main;
+
+import java.util.ArrayList;
 
 public class CreateUpdateUserController {
 
     private UserDAO userDAO;
     private DBAccess dbAccess;
+    private ArrayList<String> allRoles;
+    private ComboBox<String> roles;
 
+    @FXML
+    private Label welcomeLabel;
     @FXML
     private TextField userNameTextField;
     @FXML
@@ -21,14 +35,36 @@ public class CreateUpdateUserController {
     private MenuButton taskMenuButton;
     @FXML
     ListView<String> roleList;
+    @FXML
+    ComboBox<String> comboBox;
+
+    public CreateUpdateUserController() {
+        this.dbAccess = Main.getDBaccess();
+        this.userDAO = new UserDAO(dbAccess);
+    }
 
 
     public void setup(User user) {
-
+//        setTaskMenuButtonRoles();
     }
 
     public void doMenu() {}
 
     public void doCreateUpdateUser() {
     }
+
+//    public void setTaskMenuButtonRoles() {
+//        this.allRoles = userDAO.getAllRoles();
+//        ObservableList<String> observableList = FXCollections.observableList(allRoles);
+//        final ComboBox comboBox = new ComboBox((ObservableList) allRoles);
+//        for (String role: allRoles) {
+////            MenuItem menuItem = new MenuItem(role);
+////            taskMenuButton.getItems().add(menuItem);
+//            comboBox.setItems((ObservableList) allRoles);
+//        }
+//    }
+//
+////    public void setTextMenuButton(ActionEvent event) {
+////        taskMenuButton.setOnAction(event -> taskMenuButton.setText(allRoles.g));
+////    }
 }
--- src/main/java/test/ShaunTest.java
+++ src/main/java/test/ShaunTest.java
@@ -6,7 +6,10 @@ package test;
 import database.mysql.CourseDAO;
 import database.mysql.DBAccess;
 import database.mysql.QuizDAO;
+import database.mysql.UserDAO;
 import model.Quiz;
+import model.User;
+import view.Main;
 
 import java.util.ArrayList;
 
@@ -15,12 +18,12 @@ public class ShaunTest {
 
     public static void main(String[] args) {
 
-        DBAccess dbAccess = new DBAccess("quizzydraw", "quizzyuser", "quizzyuserPW");
-        dbAccess.openConnection();
+        //Heb een kleine aanpassing gedaan wb de DBAccess. Ik heb de nieuwe declaratie weggehaald.
+        //in plaats daarvan heb ik de methode Main.getDBaccess() gebruikt en ook neergezet in jouw tests.
 
 
         // 1. Test voor get all Quiz uit database
-        QuizDAO quizDAO = new QuizDAO(dbAccess);
+        QuizDAO quizDAO = new QuizDAO(Main.getDBaccess());
         ArrayList<Quiz> alleQuizen = quizDAO.getAll();
         for (Quiz quiz : alleQuizen){
             System.out.println(quiz);
@@ -31,19 +34,18 @@ public class ShaunTest {
         System.out.println(quiz2);
 
 
+        // Sorry pik, ik heb ze ff uit gecomment want anders wilde mn test niet runnen :)
 
         //3. Test voor set nieuwe quiz
-        CourseDAO courseDAO = new CourseDAO(dbAccess);
-        QuizDAO quizDAO1 = new QuizDAO(dbAccess);
-        Quiz quiz3 = new Quiz("Taalkunde",6,courseDAO.getOneById(1).getIdCourse());
-        quizDAO1.storeOne(quiz3);
+//        CourseDAO courseDAO = new CourseDAO(Main.getDBaccess());
+//        QuizDAO quizDAO1 = new QuizDAO(Main.getDBaccess());
+//        Quiz quiz3 = new Quiz("Taalkunde",6,courseDAO.getOneById(1).getIdCourse());
+//        quizDAO1.storeOne(quiz3);
 
-
-
-
-
-
-
+        //Test Branko --> alle rollen uit de DB krijgen
+        UserDAO userDAO = new UserDAO(Main.getDBaccess());
+        ArrayList<String> roles = userDAO.getAllRoles();
+        System.out.println(roles);
 
 
         }
