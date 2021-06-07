04437016ba332f391a82ec0f4ea44fbd70627a97
Fiona Lampers <fiona.lampers@hva.nl>
2021-06-07 22:34:09 +0200

    Fill QuizResults Class

--- src/main/java/model/QuizResult.java
+++ src/main/java/model/QuizResult.java
@@ -1,3 +1,24 @@
 package model;
 
-public class QuizResult {}
+import java.time.LocalDateTime;
+
+public class QuizResult {
+    private int takeTestNumber;
+    private int idQuiz;
+    private int idGebruiker;
+    private int numberAnswersRight;
+    private LocalDateTime dateTimeQuiz;
+
+    public QuizResult(int takeTestNumber, int idQuiz, int idGebruiker, int numberAnswersRight, LocalDateTime dateTimeQuiz) {
+        this.takeTestNumber = takeTestNumber;
+        this.idQuiz = idQuiz;
+        this.idGebruiker = idGebruiker;
+        this.numberAnswersRight = numberAnswersRight;
+        this.dateTimeQuiz = dateTimeQuiz;
+    }
+
+
+
+
+
+}
