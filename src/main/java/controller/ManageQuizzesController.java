71520ec2ae3637b8482b1c90570589a5466b3a28
Shaun van Ouwerkerk <shaun.van.ouwerkerk@hva.nl>
2021-06-07 13:52:40 +0200

    eerste opzet ManageQuizesController

--- src/main/java/controller/ManageQuizzesController.java
+++ src/main/java/controller/ManageQuizzesController.java
@@ -1,14 +1,69 @@
 package controller;
+/*
+@ Author Shaun
+Nog niet af!
+ */
+
+import database.mysql.DBAccess;
+import database.mysql.QuizDAO;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+import javafx.scene.control.TextField;
+import model.Quiz;
+import view.Main;
+import java.util.ArrayList;
+
 
 public class ManageQuizzesController {
 
-    public void setup() {}
+    private QuizDAO quizDAO;
+    private DBAccess dbAccess;
 
-    public void doMenu(){}
+    @FXML
+    ListView<Quiz> quizList;
+    @FXML
+    TextField warningText;
 
-    public void doCreateQuiz(){}
+    public ManageQuizzesController() {this.dbAccess = Main.getDBaccess();}
 
-    public void doUpdateQuiz(){}
 
-    public void doDeleteQuiz(){}
+    public void setup() {
+        this.quizDAO = new QuizDAO(dbAccess);
+        ArrayList<Quiz> allQuizes = quizDAO.getAll();
+        for (Quiz quiz : allQuizes){
+            quizList.getItems().add(quiz);
+        }
+//         Om een nullpointer exception te vermijden
+        quizList.getSelectionModel().selectFirst();
+
+
+    }
+
+    public void doMenu(){Main.getSceneManager().showWelcomeScene();}
+
+
+    public void doCreateQuiz(){};
+
+
+    public void doUpdateQuiz() {
+        Quiz quiz = quizList.getSelectionModel().getSelectedItem();
+
+        if (quiz == null) {
+            warningText.setVisible(true);
+            warningText.setText("Je dient eerst een quiz te kiezen");
+        } else {
+            Main.getSceneManager().showCreateUpdateQuizScene(quizList.getSelectionModel().getSelectedItem());
+        }
+    }
+
+    public void doDeleteQuiz(){
+        // TODO: 06/06/2021 Gaat dit met een DAO die lijnen verwijdered?
+//        Quiz quiz = quizList.getSelectionModel().getSelectedItem();
+//
+//        if (quiz == null) {
+//            warningText.setVisible(true);
+//            warningText.setText("Je dient eerst een quiz te kiezen");
+//        } else { //Nog toevoegen.
+    }
 }
