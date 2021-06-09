de79e2f8fa72a4c405c79523a4faa7589468885c
Branko Visser <b.visser@kwigy.com>
2021-06-09 15:31:30 +0200

    ok ik snap toch niet wat ik nu moet doen met testen

--- test/database/mysql/UserDAOTest.java
+++ test/database/mysql/UserDAOTest.java
@@ -1,16 +1,30 @@
 package database.mysql;
 
+import model.User;
 import org.junit.jupiter.api.Test;
+import view.Main;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserDAOTest {
 
-    @Test
-    void getOneById() {
-    }
+    private final DBAccess dbAccess = Main.getDBaccess();
+    private UserDAO userDAO = new UserDAO(dbAccess);
+    private User testUser = new User("grover", "SuperGrover", Main.STUDENT_ROL);
+
 
     @Test
     void storeOne() {
+//        userDAO.storeOne(testUser);
+    }
+
+    int testUserId = testUser.getIdUser();
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getOneById(int testUserId) {
     }
 }
