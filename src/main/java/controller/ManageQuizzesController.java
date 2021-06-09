6c2b8b105dcaa5fa0dba3229415c906bf6d56eb8
Shaun van Ouwerkerk <shaun.van.ouwerkerk@hva.nl>
2021-06-09 17:00:00 +0200

    Shaun toevoeging

--- src/main/java/controller/ManageQuizzesController.java
+++ src/main/java/controller/ManageQuizzesController.java
@@ -22,7 +22,7 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 
-public class ManageQuizzesController {
+public class  ManageQuizzesController {
 
     private QuizDAO quizDAO;
     private QuestionDAO questionDAO;
