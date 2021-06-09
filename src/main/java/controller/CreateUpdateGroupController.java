fcd92e95fc9a627a07dd73428b14783b52f66bca
Fiona Lampers <fiona.lampers@hva.nl>
2021-06-09 16:02:08 +0200

    GroupController filled

--- src/main/java/controller/CreateUpdateGroupController.java
+++ src/main/java/controller/CreateUpdateGroupController.java
@@ -1,12 +1,16 @@
 package controller;
 
 import model.Group;
+import view.Main;
 
 public class CreateUpdateGroupController {
 
     public void setup(Group group) {}
 
-    public void doMenu() {}
+    public void doMenu() {
+        Main.getSceneManager().showWelcomeScene();
+    }
+
 
     public void doCreateUpdateGroup() {}
 }
