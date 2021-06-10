eda10e921239d251c451b826acb21a569a78e32d
Nijad Nazarli <nijad.nazarli@hva.nl>
2021-06-10 22:17:18 +0200

    Question Unit Test

---
+++ test/model/QuestionTest.java
@@ -0,0 +1,17 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuestionTest {
+
+    @Test
+    void testToString() {
+        Question testVraag = new Question(10, "Wat is het hoogste punt van Nederland?",
+                "De Vaalserberg", "Vaalseberg", "De Vaalberg", "De Vaalsebert");
+        String actual = testVraag.toString();
+        String expected = "Wat is het hoogste punt van Nederland?";
+        assertEquals(expected, actual);
+    }
+}
