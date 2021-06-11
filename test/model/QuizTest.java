df2cd4d52bd057b929ee9a53d62434d8942ce4db
Shaun van Ouwerkerk <shaun.van.ouwerkerk@hva.nl>
2021-06-11 11:33:42 +0200

    Test Quiz toegevoegd

---
+++ test/model/QuizTest.java
@@ -0,0 +1,22 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuizTest {
+
+    @Test
+    void getIdQuiz() {
+        Quiz quiz1 = new Quiz("Pubquiz",8,8);
+        int actual = quiz1.getIdQuiz();
+        int expected = 0;
+    }
+
+    @Test
+    void getIdCourse() {
+        Quiz quiz2 = new Quiz("Pubquiz2",5);
+        int actual = quiz2.getIdCourse();
+        int expected = 0;
+    }
+}
