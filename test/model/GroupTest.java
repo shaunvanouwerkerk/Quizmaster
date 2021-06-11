2b590a926dce3a4d902cc4be308f26ef1a3b6905
Fiona Lampers <fiona.lampers@hva.nl>
2021-06-11 12:48:47 +0200

    GroupTest

---
+++ test/model/GroupTest.java
@@ -0,0 +1,25 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GroupTest {
+
+    @Test
+    void testToString() {
+        Group testGroup= new Group(1, 1,1);
+        String actual = testGroup.toString();
+        String expected = "Group id: 1";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getIdGroup() {
+    }
+
+    @Test
+    void setIdGroup() {
+    }
+
+}
