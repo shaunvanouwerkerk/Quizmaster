f0ec04e19318837fcbbc5559aafc648c2a688471
Shaun van Ouwerkerk <shaun.van.ouwerkerk@hva.nl>
2021-06-11 12:11:53 +0200

    Aanpassing test Quiz

--- test/model/QuizTest.java
+++ test/model/QuizTest.java
@@ -11,6 +11,7 @@ class QuizTest {
         Quiz quiz1 = new Quiz("Pubquiz",8,8);
         int actual = quiz1.getIdQuiz();
         int expected = 0;
+        assertEquals(expected,actual);
     }
 
     @Test
@@ -18,5 +19,6 @@ class QuizTest {
         Quiz quiz2 = new Quiz("Pubquiz2",5);
         int actual = quiz2.getIdCourse();
         int expected = 0;
+        assertEquals(expected,actual);
     }
 }
