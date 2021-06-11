7eb46d33e6da6895b97b0ca382b57566332349e4
Branko Visser <b.visser@kwigy.com>
2021-06-11 12:01:55 +0200

    aantal gebruikers met dezelfde rol toegevoegd

--- src/main/java/controller/ManageUsersController.java
+++ src/main/java/controller/ManageUsersController.java
@@ -5,7 +5,9 @@ import database.mysql.UserDAO;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.text.Text;
 import model.User;
 import view.Main;
 
@@ -14,11 +16,16 @@ import java.util.Optional;
 
 public class ManageUsersController {
 
+
     private UserDAO userDAO;
     private DBAccess dbAccess;
+    private ArrayList<User> allUsers;
+
 
     @FXML
-    ListView<User> userList;
+    private ListView<User> userList;
+    @FXML
+    private Label aantalGebruikers;
 
     public ManageUsersController() {
         this.dbAccess = Main.getDBaccess();
@@ -27,11 +34,15 @@ public class ManageUsersController {
 
     public void setup() {
         this.userDAO = new UserDAO(dbAccess);
-        ArrayList<User> allUsers = userDAO.getAll();
+        allUsers = userDAO.getAll();
         for(User user : allUsers) {
             userList.getItems().add(user);
         }
         userList.getSelectionModel().selectFirst();
+        userList.setOnMouseClicked(mouseEvent -> setLabelRoleCount(userList.getSelectionModel()
+                .getSelectedItem().getRoleName()));
+        userList.setOnKeyPressed(keyEvent -> setLabelRoleCount(userList.getSelectionModel()
+                .getSelectedItem().getRoleName()));
 
     }
 
@@ -57,5 +68,15 @@ public class ManageUsersController {
         if(result.get() == ButtonType.OK) {
             userDAO.deleteUser(userList.getSelectionModel().getSelectedItem());
         }
+    }
+
+    public void setLabelRoleCount(String role) {
+        int sumRole = 0;
+        for(User user : allUsers) {
+            if(user.getRoleName().equals(role)) {
+                sumRole++;
+            }
+        }
+        aantalGebruikers.setText(String.format("Het aantal gebruikers met de rol %s is %d", role, sumRole));
     }
 }
