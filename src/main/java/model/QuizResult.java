80f439a9f2f0022e7e07f10b681528f8f4511563
Fiona Lampers <fiona.lampers@hva.nl>
2021-06-15 11:01:00 +0200

    QuizResults

--- src/main/java/model/QuizResult.java
+++ src/main/java/model/QuizResult.java
@@ -9,7 +9,8 @@ public class QuizResult {
     private int numberAnswersRight;
     private LocalDateTime dateTimeQuiz;
 
-    public QuizResult(int takeTestNumber, int idQuiz, int idGebruiker, int numberAnswersRight, LocalDateTime dateTimeQuiz) {
+    public QuizResult(int takeTestNumber, int idQuiz, int idGebruiker, int numberAnswersRight,
+                      LocalDateTime dateTimeQuiz) {
         this.takeTestNumber = takeTestNumber;
         this.idQuiz = idQuiz;
         this.idGebruiker = idGebruiker;
@@ -17,8 +18,47 @@ public class QuizResult {
         this.dateTimeQuiz = dateTimeQuiz;
     }
 
+    public QuizResult(int idQuiz, int idGebruiker, int numberAnswersRight, LocalDateTime dateTimeQuiz) {
+        this(0, idQuiz, idGebruiker, numberAnswersRight, dateTimeQuiz);
+    }
 
+    public int getTakeTestNumber() {
+        return takeTestNumber;
+    }
 
+    public void setTakeTestNumber(int takeTestNumber) {
+        this.takeTestNumber = takeTestNumber;
+    }
 
+    public int getIdQuiz() {
+        return idQuiz;
+    }
 
+    public void setIdQuiz(int idQuiz) {
+        this.idQuiz = idQuiz;
+    }
+
+    public int getIdGebruiker() {
+        return idGebruiker;
+    }
+
+    public void setIdGebruiker(int idGebruiker) {
+        this.idGebruiker = idGebruiker;
+    }
+
+    public int getNumberAnswersRight() {
+        return numberAnswersRight;
+    }
+
+    public void setNumberAnswersRight(int numberAnswersRight) {
+        this.numberAnswersRight = numberAnswersRight;
+    }
+
+    public LocalDateTime getDateTimeQuiz() {
+        return dateTimeQuiz;
+    }
+
+    public void setDateTimeQuiz(LocalDateTime dateTimeQuiz) {
+        this.dateTimeQuiz = dateTimeQuiz;
+    }
 }
