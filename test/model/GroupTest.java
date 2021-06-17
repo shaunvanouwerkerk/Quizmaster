f199fab0af8776034f822333b48e47d306c829bc
Fiona Lampers <fiona.lampers@hva.nl>
2021-06-17 10:20:55 +0200

    expand Unittest for Group

--- test/model/GroupTest.java
+++ test/model/GroupTest.java
@@ -1,6 +1,9 @@
 package model;
 
 import org.junit.jupiter.api.Test;
+import view.Main;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,18 +11,45 @@ class GroupTest {
 
     @Test
     void testToString() {
-        Group testGroup= new Group(1, "ProgGr1", 1,1);
-        String actual = testGroup.toString();
-        String expected = "Group id: 1";
+        Group group1= new Group(1, "AA", 1, 1);
+        String actual = group1.toString();
+        String expected = "AA";
         assertEquals(expected, actual);
     }
 
+
     @Test
     void getIdGroup() {
+        Group group1= new Group(1, "AA", 1, 1);
+        int actual = group1.getIdCourse();
+        int expected = 1;
+        assertEquals(expected,actual);
     }
 
     @Test
-    void setIdGroup() {
+    void getNameGroup() {
+        Group group1= new Group(1, "AA", 1, 1);
+        String actual = group1.getNameGroup();
+        String expected ="AA";
+        assertEquals(expected,actual);
     }
+
+    @Test
+    void getIdCoordinator() {
+        Group group2= new Group(1, "AA", 2, 1);
+        int actual = group2.getIdCooridnator();
+        int expected = 2;
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void getIdCurse() {
+        Group group2= new Group(1, "AA", 2, 1);
+        int actual = group2.getIdCourse();
+        int expected = 1;
+        assertEquals(expected,actual);
+    }
+
+
 
 }
